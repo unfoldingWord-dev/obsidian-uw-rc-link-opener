ade1eaabce99f225b6a43c39922d571f00d10bbb
Richard Mahn <richmahn@users.noreply.github.com>
2021-06-04 16:59:10 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,121 @@
+'use strict';
+
+var obsidian = require('obsidian');
+var child_process = require('child_process');
+var fs = require('fs');
+var path = require('path');
+var os = require('os');
+
+function _interopNamespace(e) {
+    if (e && e.__esModule) return e;
+    var n = Object.create(null);
+    if (e) {
+        Object.keys(e).forEach(function (k) {
+            if (k !== 'default') {
+                var d = Object.getOwnPropertyDescriptor(e, k);
+                Object.defineProperty(n, k, d.get ? d : {
+                    enumerable: true,
+                    get: function () {
+                        return e[k];
+                    }
+                });
+            }
+        });
+    }
+    n['default'] = e;
+    return Object.freeze(n);
+}
+
+var path__namespace = /*#__PURE__*/_interopNamespace(path);
+
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation.
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
+***************************************************************************** */
+
+function __awaiter(thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+}
+
+class UwRcLinkPlugin extends obsidian.Plugin {
+    get profiles() {
+        return Object.assign(Object.assign({}, this.presetProfiles), this.settings.custom);
+    }
+    onload() {
+        return __awaiter(this, void 0, void 0, function* () {
+            this.registerDomEvent(document, 'click', (evt) => __awaiter(this, void 0, void 0, function* () {
+                const ele = evt.target;
+                evt.preventDefault();
+                const url = ele.getAttribute('href');
+                if (url && url.startsWith('rc://')) {
+                    evt.preventDefault();
+                    const parts = url.split("/");
+                    const filePathToOpen = [parts[2] + "_" + parts[3]].concat(parts.slice(5)).join("/");
+                    console.log("RC link "+url+ " converted to " + filePathToOpen);
+                    yield this.app.workspace.openLinkText(filePathToOpen, '', true, { active: true });
+                    return false
+                }
+            }));
+        });
+    }
+}
+class PanicModal extends obsidian.Modal {
+    constructor(app, message) {
+        super(app);
+        this.message = message;
+    }
+    onOpen() {
+        let { contentEl } = this;
+        contentEl.setText(this.message);
+    }
+    onClose() {
+        let { contentEl } = this;
+        contentEl.empty();
+    }
+}
+class SettingTab extends obsidian.PluginSettingTab {
+    constructor(app, plugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+        this._profileChangeHandler = obsidian.debounce((val) => __awaiter(this, void 0, void 0, function* () {
+            var _a, _b;
+            try {
+                const profiles = JSON.parse(val);
+                this.plugin.settings.custom = profiles;
+                yield this.plugin.saveSettings();
+                this._render();
+            }
+            catch (e) {
+                this.panic((_b = (_a = e.message) !== null && _a !== void 0 ? _a : e.toString()) !== null && _b !== void 0 ? _b : 'some error occurred in open-link-with');
+            }
+        }), 1500, true);
+    }
+    panic(msg) {
+        new PanicModal(this.app, msg).open();
+    }
+    _render() {
+        let { containerEl } = this;
+        containerEl.empty();
+    }
+    display() {
+        this._render();
+    }
+}
+
+module.exports = UwRcLinkPlugin;
