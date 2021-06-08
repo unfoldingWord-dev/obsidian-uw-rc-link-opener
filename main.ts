7401ffa10b526114fd69346ce335a5e3c465d6fc
Richard Mahn <richmahn@users.noreply.github.com>
2021-06-08 13:40:33 -0400

    Support for UTA

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 import { Plugin } from 'obsidian';
+import * as path from 'path';
 
 export default class UwRcLinkOpener extends Plugin {
 	async onload() {
@@ -10,9 +11,13 @@ export default class UwRcLinkOpener extends Plugin {
         evt.preventDefault();
         const url = ele.getAttribute('href');
         if (url && url.startsWith('rc://')) {
-        	evt.preventDefault();
+            evt.preventDefault();
             const parts = url.split("/");
-            const filePathToOpen = [parts[2] + "_" + parts[3]].concat(parts.slice(5)).join("/");
+            const resource = parts[3];
+            var filePathToOpen = [parts[2] + "_" + resource].concat(parts.slice(5)).join("/");
+            if (resource == 'ta') {
+                filePathToOpen = path.join(filePathToOpen, '01.md');
+            }
             console.log("RC link "+url+ " converted to " + filePathToOpen);
             this.app.workspace.openLinkText(filePathToOpen, '', true, { active: true });
             return false;
